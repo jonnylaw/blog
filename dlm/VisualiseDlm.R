580f5fc4731b1aea1363709cbe30dc52b0d77633
lawsy <law.jonny@googlemail.com>
2016-04-17 15:24:13 +0100

    Added visualisation for DLM filter

--- dlm/VisualiseDlm.R
+++ dlm/VisualiseDlm.R
@@ -31,11 +31,15 @@ filtered %<>%
   mutate(upper = qnorm(p = 0.975, mean = m, sd = sqrt(c)), 
          lower = qnorm(p = 0.025, mean = m, sd = sqrt(c)))
 
+## join to the original data to see how close the Kalman Filter estimates the state
+filtered %<>%
+  inner_join(data[,-3], by = c("time", "stationId")) %>%
+  select(-v, -w, -m, -c, -observation) %>%
+  gather(key = "key", value = "value", -time, -stationId)
+
 ## Plot state estimate and intervals
 filtered %>%
-  select(-v, -w, -c) %>%
-  gather(key = "key", value = "value", -time, -stationId) %>%
   filter(stationId %in% 1:4) %>%
-  ggplot(aes(x = time, y = value, colour = key)) + 
+  ggplot(aes(x = time, y = value, colour = key, linetype = key)) + 
   geom_line() +
   facet_wrap(~stationId, scales = "free")
